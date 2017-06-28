8348fa6e8666cdca96903fab44f21ae8403368a8
Javier López <sjavierlopez@gmail.com>
2017-06-28 00:34:32 -0500

    working with ExecutorServices

--- src/com/jav/lopez/Main.java
+++ src/com/jav/lopez/Main.java
@@ -1,6 +1,9 @@
 package com.jav.lopez;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
@@ -11,21 +14,26 @@ public class Main {
         String[] inFiles = {"files/file1.txt", "files/file2.txt", "files/file3.txt", "files/file4.txt", "files/file5.txt"};
         String[] outFiles = {"files/file1.out.txt", "files/file2.out.txt", "files/file3.out.txt", "files/file4.out.txt", "files/file5.out.txt"};
 
-        Thread[] threads = new Thread[inFiles.length];
+        //Thread[] threads = new Thread[inFiles.length];
+
+        ExecutorService es = Executors.newFixedThreadPool(3);
 
 
 
         for (int i = 0; i < inFiles.length; i++) {
 
             Adder adder = new Adder(inFiles[i], outFiles[i]);
-            threads[i] = new Thread(adder);
-            threads[i].start();
+            es.submit(adder);
+
+            //threads[i] = new Thread(adder);
+            //threads[i].start();
         }
 
+
         try {
-            for (Thread thread : threads) {
-                thread.join();
-            }
+            es.shutdown();
+            es.awaitTermination(60, TimeUnit.SECONDS);
+
         }catch (InterruptedException e) {
             System.out.println("the application is failing " + e.getMessage());
         }
